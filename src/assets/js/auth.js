a37bf61ccc2aac692f4536f3cf89928993f65d99
valeskaperezrretamal <jperezr@udd.cl>
2019-04-05 01:15:20 -0300

    se agrega funcion login

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,24 +1,6 @@
-/*
-1- Crearemos dos funciones que silumarán el login con Google y la creación de cuenta
-*/
-//export para exportar la función
-//export const  = () => {
-  //  let email = document.getElementById('email').value;
-   // let contrasena = document.getElementById('contrasena').value
-  //return  firebase.auth().createUserWithEmailAndPassword(email, contrasena) 
-  //.catch(function(error) {
-    // Handle Errors here.
-    //var errorCode = error.code;
-    //var errorMessage = error.message;
-    // ...
- // });;
-//}
+import {templateLogin} from './../views/templateLogin.js';
 
-export const createAccount = () => {
-    let mail = document.getElementById('email').value;
-    let password = document.getElementById('contrasena').value;
-    console.log(mail);
-    console.log(password);
+export const createAccount = (mail,password) => {
     firebase.auth().createUserWithEmailAndPassword(mail, password)
     .catch(function(error) {// catch es agarrar(este agarra error)
     // maneja los errores aqui.
@@ -53,13 +35,24 @@ export const loginGoogle =() => {
 }
 export const logout=()=>{
   firebase.auth().signOut().then(function() {
-   // Cierre de sesión exitoso.
+    document.getElementById('root').appendChild(templateLogin());
+
   }).catch(function(error) {
     // Ha ocurrido un error.
   });
+};
+
+export const login=(email,password)=>{
+firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode === 'auth/wrong-password') {
+    
+  } else {
+    
+  }
+  
+});
 }
-//////////función de ejemplo para test////////////
-export const suma = (a,b) => {
-  return a+b;
-}
-//////////////////////////////////////////////////
