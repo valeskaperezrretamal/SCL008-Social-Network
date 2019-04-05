8dd996f42d1d06aa2f95e58a9676208ea220819c
valeskaperezrretamal <jperezr@udd.cl>
2019-04-05 01:19:58 -0300

    se modifica para que se muestre el menu solo si el usuario esta logueado

--- src/assets/views/templateMenu.js
+++ src/assets/views/templateMenu.js
@@ -1,13 +1,19 @@
 export const templateMenuBottom = () => {
-    //creamos div que contendrá la plantilla
+    
+
+  firebase.auth().onAuthStateChanged(function(user) {
     const container = document.getElementById("div-menu-bottom");
     const content =  `<ul id="menu-bottom">
                                    
                              <li><a href="#/profile">Mi perfil</a></li>
                               <li><a href="#/articles">Temas de Interes</a></li>
+                              <li><a href="#/logout">cerrar sesion</a></li>
                             </ul>`
-                         //aqui muestro el codigo para que se vea mi pantalla
+    if (user) {
+      container.innerHTML = content;
+    } else {
+      container.innerHTML = "";
+    }
+  });
     
-    //pasar el contenido al div
-    container.innerHTML = content;
 };
