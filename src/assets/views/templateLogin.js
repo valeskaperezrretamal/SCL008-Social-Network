908d4bb8de2e6f5a56f0fec600152af5eb23a308
valeskaperezrretamal <jperezr@udd.cl>
2019-04-05 01:19:12 -0300

    se agrega enlace a loginwidtoutgoogle

--- src/assets/views/templateLogin.js
+++ src/assets/views/templateLogin.js
@@ -6,7 +6,7 @@ Debemos importar la función loginGoogle()
 */
 //importar y exportar las vistas, para que se muestren en html
 //exportar se va a utilizar de otro archivo
-import {loginGoogle} from './../js/auth.js'; // es para ingresar al archivo js  
+  
 
 export const templateLogin = () => {
 //creamos div que contendrá en plantilla
@@ -21,33 +21,25 @@ const contentLogin = `
                            </div>
                            <div class="green">
                            <ul id = "tabla">
-                                <li> <a >inicia sesion</a>  
+                                <li> <a href="#/loginwithoutgoogle">inicia sesion</a>  
+
 
                                 </li>
                                <li>
-                                   <a href="#/logingoogle" id="logingoogle">Ingreso Usuario con Google</a>
+                                   <a href="#/logingoogle">Ingreso Usuario con Google</a>
                                </li>
                                <li>
                                    <a href="#/create">Registro Usuario</a>
                                </li>
                            </ul>
                            
-                           <ul id="menu-bottom">
-                                   
-                                   <li><a href="#/profile">Mi perfil</a></li>
-                                   <li><a href="#/articles">Temas de Interes</a></li>
-                           </ul>
+                           
                            </div>`;
 
 //pasar el contenido al div
 containerLogin.innerHTML = contentLogin;
-const btn = containerLogin.querySelector('#logingoogle'); 
-// que busque el id que tiene este #login, es para que busque lo que yo quiero que busque, aun que no sea documento, hasta en hijoss
 
-//evento del botón que llama a la autentificación del google.
-btn.addEventListener('click',() => {
-    loginGoogle();
-})
 //solo 1 vista, la que va a tener el boton con google
 return containerLogin; 
 }
+
