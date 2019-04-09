58b817765f3de3b48ede89c9a1ea66e65fee24aa
lucitajulio <lucitajulio@hotmail.com>
2019-04-09 11:35:02 -0400

    validando  mas datos de usuario, direccion,ciudad,nombre y creando en autentificacion para firebase

--- src/assets/views/templateCreate.js
+++ src/assets/views/templateCreate.js
@@ -18,11 +18,13 @@ export const templateCreate = () => {
                          <hr>
                          
                           <input id="nombre" type="text" placeholder="Nombre Completo">
+                          <p id="errornombre"></p>
                           <input id="direccion" type="text" placeholder="Direccion Completa">
+                          <p id="errordireccion"></p>
                           <input id="ciudad" type="text" placeholder="Indica tu ciudad">
-                           
+                          <p id="errorciudad"></p>
                           <input id="email" type="email" placeholder="Ingresa email">
-                            <p id="erroremail"></p>
+                          <p id="erroremail"></p>
                            <input id="contrasena" type="password" placeholder="Ingresa contraseña">
                            <p id= "errorpassword"></p>
                            <button id= "create">Registrar</button>
@@ -33,6 +35,9 @@ export const templateCreate = () => {
 
     containerCreate.innerHTML = contentCreate;
     containerCreate.querySelector('#create').addEventListener('click',() => {
+    let name = document.getElementById('nombre').value;
+    let address = document.getElementById('direccion').value;
+    let city = document.getElementById('ciudad').value; //agarrando valores de id
     let mail = document.getElementById('email').value;
     let password = document.getElementById('contrasena').value;
         
@@ -40,10 +45,19 @@ export const templateCreate = () => {
             document.getElementById("erroremail").innerHTML=`Debes ingresar un correo, no puede estar vacio este campo` //para pasar info. al html
         
         }
+        if(name===""){
+            document.getElementById("errornombre").innerHTML=`debe ingresar nombre`
+        }
+        if(address===""){
+            document.getElementById("errordireccion").innerHTML=`debe ingresar direccion valida`
+        }
+        if(city===""){
+            document.getElementById("errorciudad").innerHTML=`debe ingresar una ciudad de chile`//muestra el error en las cajas vacias
+        }
         if(password===""){
             document.getElementById("errorpassword").innerHTML=`Debes ingresar una contraseña, no puede estar vacio este campo`
         }
-        if(emptyKey(mail,password)) createAccount(mail,password); 
+        if(emptyKey(name,address,city,mail,password)) createAccount(name,address,city,mail,password); //se esta pasando a la funcion emptykey para que me las valide, con la funcion de validar
     });
     //solo 1 vista, la que va a tener el boton con google
     return containerCreate; 
